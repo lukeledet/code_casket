571c1b63cfe1d5828320f18b7cf965745b1d52be
Luke Ledet <luke@lootbox.org>
2016-01-10 11:59:34 -0600

    Creating codes :(

---
+++ web/controllers/code_controller.ex
@@ -0,0 +1,25 @@
+defmodule CodeCasket.CodeController do
+  use CodeCasket.Web, :controller
+
+  alias CodeCasket.Code
+
+  plug :scrub_params, "code" when action in [:create, :update]
+
+  def index(conn, _params) do
+    changeset = Code.changeset(%Code{})
+    render conn, "index.html", changeset: changeset, codes: codes
+  end
+
+  def create(conn, %{"code" => code_params}) do
+    changeset = Code.changeset(%Code{}, code_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _code} ->
+        conn
+        |> put_flash(:info, "Code created successfully.")
+        |> redirect(to: code_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "index.html", changeset: changeset)
+    end
+  end
+end
---
+++ web/views/code_view.ex
@@ -0,0 +1,3 @@
+defmodule CodeCasket.CodeView do
+  use CodeCasket.Web, :view
+end
