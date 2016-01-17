ab959f7ca78c57a22dcb0a08e11b91d79fa9cd41
Luke Ledet <luke@lootbox.org>
2016-01-16 20:01:48 -0600

    Show your stupid code when you make it

--- web/controllers/code_controller.ex
+++ web/controllers/code_controller.ex
@@ -10,14 +10,19 @@ defmodule CodeCasket.CodeController do
     render conn, "index.html", changeset: changeset
   end
 
+  def show(conn, %{"id" => id}) do
+    code = Repo.get(Code, id)
+    render conn, "show.html", code: code
+  end
+
   def create(conn, %{"code" => code_params}) do
     changeset = Code.changeset(%Code{}, code_params)
 
     case Repo.insert(changeset) do
-      {:ok, _code} ->
+      {:ok, code} ->
         conn
         |> put_flash(:info, "Code created successfully.")
-        |> redirect(to: code_path(conn, :index))
+        |> redirect(to: code_path(conn, :show, code.id))
       {:error, changeset} ->
         render(conn, "index.html", changeset: changeset)
     end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule CodeCasket.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", CodeController, :index
+    get "/codes/:id", CodeController, :show
     post "/codes", CodeController, :create
   end
 
